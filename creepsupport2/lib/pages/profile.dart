d0e8e302beefdcb010fb4fb9b67217354fe6b6dc
aimbesi1 <tonyimbesi2@gmail.com>
2022-07-05 00:20:27 -0400

    Update profile.dart

--- creepsupport2/lib/pages/profile.dart
+++ creepsupport2/lib/pages/profile.dart
@@ -1,3 +1,6 @@
+import 'package:creepsupport2/services/firestore_service.dart';
+
+import '../model/post.dart';
 import '../pages/authentication.dart';
 import '../pages/home.dart';
 import '../model/user.dart';
@@ -13,13 +16,48 @@ class Profile extends StatefulWidget {
 }
 
 class _ProfileState extends State<Profile> {
+  final FirestoreService _fs = FirestoreService();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text(widget.observedUser.name)),
-      body: Center(
-        child: Text(widget.observedUser.bio),
-      ),
+      appBar: AppBar(
+          title:
+              Text("${widget.observedUser.name}: ${widget.observedUser.bio}")),
+      body: StreamBuilder<List<Post>>(
+          stream: _fs.post,
+          builder: (BuildContext context, AsyncSnapshot<List<Post>> snapshot) {
+            if (snapshot.hasError) {
+              return const Center(
+                child: Text("Error occurred while loading posts"),
+              );
+            } else if (snapshot.hasData) {
+              var posts = snapshot.data!;
+
+              // Only show posts whose IDs match the viewed post
+              var filterpost = [];
+              for (var element in posts) {
+                if (element.authorID == widget.observedUser.id) {
+                  filterpost.add(element);
+                }
+              }
+              return filterpost.isEmpty
+                  ? const Center(child: Text("No posts yet"))
+                  : ListView.builder(
+                      itemCount: filterpost.length,
+                      itemBuilder: (BuildContext context, int index) =>
+                          ListTile(
+                            title: Text(widget.observedUser.name),
+                            subtitle: Column(
+                                mainAxisAlignment: MainAxisAlignment.start,
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text(filterpost[index].content),
+                                  const SizedBox(height: 10),
+                                ]),
+                          ));
+            }
+            return const Center(child: Text("No data yet"));
+          }),
     );
   }
 }
